346bafc975b181baa84519d25e416521d2f74122
AbdulazizMohammad <opomhmd@hotmail.com>
2023-04-05 14:35:11 +0300

    add database config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:arabic_learning_game/views/main_menu_view.dart';
+import 'package:arabic_learning_game/db_servieces.dart';
 
-void main() {
+late DatabaseService databaseService;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  databaseService = DatabaseService();
+  await databaseService.prepareDatabases();
   runApp(const MyApp());
 }
